aa0a2f16e430cbbfd62019888867e130cee7c579
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-02-27 15:55:28 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+import AppMain from "./components/AppMain.vue";
+export default {
+  data() {
+    return {
+      title: "Hello world!!",
+    };
+  },
+
+  components: { AppHeader, AppFooter, AppMain },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Header",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style></style>
