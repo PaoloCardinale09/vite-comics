86d38d37217152f0c7fad76ee00c8d8bb5022fcd
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-02-28 17:48:11 +0100

    commit

---
+++ src/components/AppCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    img: String,
+    title: String,
+  },
+};
+</script>
+<template>
+  <div class="container">
+    <div class="card">
+      <img :src="img" alt="img" />
+      <div class="title">
+        <h2>{{ title }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.card {
+  width: calc(100% / 6);
+  height: 12rem;
+  padding: 1rem;
+  color: white;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+  }
+  h2 {
+    font-size: 1rem;
+  }
+}
+</style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Jumbotron",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<style lang="scss" scoped>
+.jumbotron {
+  height: 18rem;
+  background-image: url(../assets/img/jumbotron.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
